5c637c1c18be9805928fbf534bf74c13f7e72191
Palevu4real <85342861+Palevu4real@users.noreply.github.com>
2021-08-07 12:32:39 +0100

    my updated

--- src/App.js
+++ src/App.js
@@ -14,10 +14,11 @@ return (
 
 <nav> 
 <ul>
-  <li><Link to="/Home">Home</Link> </li>
-  <li><Link to="/About">About</Link> </li>
+<li><Link to="/Login">Login</Link></li>
+<li><Link to="/About">About</Link> </li>
   <li><Link to="/Contact">Contact</Link> </li>
-  <li><Link to="/Login">Login</Link></li>
+  <li><Link to="/Home">Home</Link> </li>
+  
   </ul>
       </nav>
   <div className="homeComponent">
